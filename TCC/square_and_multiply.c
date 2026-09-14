d5d1234d0e0a224fccf2d44e31823f11adcdc051
André Garrido <andregarridomacedo@gmail.com>
2026-09-13 23:18:47 -0300

    Create square_and_multiply.c
    criação da base do codigo + anotações

---
+++ TCC/square_and_multiply.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define p 6399395417 //teste primos com 10 digitos
+#define q 2196825613
+
+/*ToDo:
+        - geração de chaves
+            -definição das chaves publicas e privadas (escolher valores primos)
+        - criptografar mensagens
+        - descriptografar
+*/
+
+int main(){
+    
+
+
+    return 0;
+}
