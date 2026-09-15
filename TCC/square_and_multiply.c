70c82eeb1e7be99f24dd7d71869ce4936d06d9a2
André Garrido <andregarridomacedo@gmail.com>
2026-09-15 07:41:22 -0300

    implementações
    assinaturas e square and multiply

--- TCC/square_and_multiply.c
+++ TCC/square_and_multiply.c
@@ -3,6 +3,9 @@
 
 #define p 6399395417 //teste primos com 10 digitos
 #define q 2196825613
+#define n p*q
+#define e (p-1) * (q-1)
+
 
 /*ToDo:
         - geração de chaves
@@ -11,9 +14,26 @@
         - descriptografar
 */
 
+
+char* encriptarMensagem(char);
+char* decriptarMensagem(char);
+int squareAndMultiply(int[] , int , char);
+
 int main(){
     
 
 
     return 0;
+}
+
+
+int squareAndMultiply(int x[], int tamanho , char* mensagem){
+    int c = 1;
+    for(int i = tamanho; i >= 0 ; i--){
+        c = ((c*c) % n); 
+        if(x[i] == 1){
+            c = ((c*mensagem[i]) % n);
+        }
+    }
+    return c;
 }
